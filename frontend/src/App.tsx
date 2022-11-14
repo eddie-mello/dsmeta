d552125f9fc073b2ba9c6040cdc6a44e5ded5626
Eddie Mello <eddie.l.m.cursos@gmail.com>
2022-11-14 17:20:57 -0300

    Other components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,17 @@
-import NotificationButton from "./components/NotificationButton";
+import Header from "./components/Header";
+import Card from "./components/SalesCard";
 
 function App() {
   return (
     <>
-      <h1>Olá mundo!</h1>
-      <NotificationButton />
-      <NotificationButton />
-      <NotificationButton />
-      <NotificationButton />
-      <NotificationButton />
-      <NotificationButton />
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <Card />
+          </div>
+        </section>
+      </main>
     </>
   )
 }
